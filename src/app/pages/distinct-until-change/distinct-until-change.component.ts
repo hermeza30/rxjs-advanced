317d44fc7bc38d80937c811578ec1f15ad378ba8
Antonio Hermez <antonio.hermez@MacBookPro-infra0177.fibertel.com.ar>
2021-08-31 17:47:55 -0300

    Distinct untiil change with keyselector

--- src/app/pages/distinct-until-change/distinct-until-change.component.ts
+++ src/app/pages/distinct-until-change/distinct-until-change.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { distinctUntilChanged } from 'rxjs/operators';
-import { of, Subscription } from 'rxjs';
+import { from, of, Subscription } from 'rxjs';
 import { obs } from 'src/app/interface';
+import { NumberSymbol } from '@angular/common';
 
 @Component({
   selector: 'app-distinct-until-change',
@@ -14,18 +15,31 @@ export class DistinctUntilChangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
     of(1,2,3,4,5,6,6,7,8,9,9,9,4,5,6).pipe(distinctUntilChanged()).subscribe(obs);//
+    
     console.log("*************");
+    
     of(1,2,3,4,5,6,7,8,9).pipe(distinctUntilChanged((prev,curr)=>{
       //Cuando el valor es true no deja pasar
       //Cada vez que el valor pasa prev toma el valor del current
       //prev=1 y current =2 entonces 2===1+1 es true entonces no deja pasar, es decir hace el skip del 2
       //y prev queda en 1 y current en 3
-      console.log('Prev',prev);
-      console.log('Curr', curr);
-      console.log("comparacion",curr===prev+1)
         return curr===prev+1;
     })).subscribe(obs)
+    
+    console.log("*************");
+
+    interface Employe{
+      id:number,
+      name:string
+    }
+    var employe:Employe[]=[{id:1,name:'tony'},{id:5,name:'tony'},{id:2,name:'elias'},{id:3,name:'padre'},{id:4,name:'mam'}]
+
+    from(employe).pipe(distinctUntilChanged((prev:Employe,curr:Employe)=>{
+      return prev===curr//Si la comparacion es true no deja pasar el current value
+    },(k:any)=>k.name)).subscribe(obs)
+
 
   }
 
