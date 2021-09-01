04792ae70a175682d0e9f86bd4e6d58d39bfdd92
Antonio Hermez <antonio.hermez@MacBookPro-infra0177.fibertel.com.ar>
2021-09-01 16:52:45 -0300

    last

--- src/app/pages/last/last.component.ts
+++ src/app/pages/last/last.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { last, tap } from 'rxjs/operators';
+import { obs } from 'src/app/interface';
 
 @Component({
   selector: 'app-last',
@@ -11,6 +14,13 @@ export class LastComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    ///First pelado
+  of('a','b','c','d','e').pipe(tap(v=>console.log('----Basic----')),last()).subscribe(obs)
+  //Last con filtro
+  of(1,2,3,4,5).pipe(tap(v=>console.log('----Una condicion----')),last(v=>v>2)).subscribe(obs)
+  //LASt con filtro canbiando la salida en caso de que no se cumpla la condicion
+  of(1,2,3,4,5).pipe(tap(v=>console.log('----Condicion false y retorno otro valor----')),last(v=>v>6,10)).subscribe(obs)
+
   }
 
 }
