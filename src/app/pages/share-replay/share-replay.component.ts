8cae3b7843622f3d9f88c458adbe75c3eb741523
Antonio Hermez <antonio.hermez@fluxit.com.ar>
2022-06-28 12:59:50 -0300

    fix: share vs shareReplay

--- src/app/pages/share-replay/share-replay.component.ts
+++ src/app/pages/share-replay/share-replay.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { fromEvent, interval } from 'rxjs';
-import { shareReplay, skipUntil, take, first, tap } from 'rxjs/operators';
+import {
+  shareReplay,
+  skipUntil,
+  take,
+  first,
+  tap,
+  share,
+  map,
+} from 'rxjs/operators';
 import { obs } from '../../interface';
 import { HttpClient } from '@angular/common/http';
 
@@ -15,8 +23,10 @@ export class ShareReplayComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
-    this.solucion();
+    // this.solucion();
     // this.problemaPlanteado()
+    // this.shareReplaySolo();
+    this.shareSolo();
   }
   problemaPlanteado() {
     /**Problema que se plantea es repetir la misma peticion varias veces, enttonces queremos hacerla una vez
@@ -37,9 +47,40 @@ export class ShareReplayComponent implements OnInit {
       result$.subscribe(obs);
     }
   }
+
   getHttpSharedReplay() {
     return this.http
       .get(`https://jsonplaceholder.typicode.com/comments/`)
       .pipe(tap(() => console.log('http shared')));
+  }
+  shareReplaySolo() {
+    const sharedReplay$ = interval(1000).pipe(take(6), shareReplay(3));
+    sharedReplay$.subscribe((value) => {
+      console.log('En la subscripcion A', value);
+    });
+    sharedReplay$.subscribe((value) => {
+      console.log('En la subscripcion B', value);
+    });
+    setTimeout(() => {
+      sharedReplay$.subscribe((value) => {
+        console.log('en la subscripcion c', value); //a los 10 segundos comparte los valores pero sin ejecutar de nuevo el observable
+      });
+    }, 10000);
+  }
+  shareSolo() {
+    const shared$ = interval(1000).pipe(
+      take(10),
+      map((x) => x * x),
+      share()
+    );
+    shared$.subscribe((value) => {
+      console.log('En la subscripcion A', value);
+    });
+
+    setTimeout(() => {
+      shared$.subscribe((value) => {
+        console.log('en la subscripcion B', value); //a los 3 segundos el observable comparte los valores generados por el observable padre.
+      });
+    }, 3000);
   }
 }
